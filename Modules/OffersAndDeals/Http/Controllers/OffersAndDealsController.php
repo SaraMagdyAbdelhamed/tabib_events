54a4ec69ff592f5e5311ed82005bc289df6fa10e
SaraMagdyAbdelhamed <sara.magdy1191995@gmail.com>
2018-07-19 17:55:32 +0200

    offers and deals

--- Modules/OffersAndDeals/Http/Controllers/OffersAndDealsController.php
+++ Modules/OffersAndDeals/Http/Controllers/OffersAndDealsController.php
@@ -5,6 +5,7 @@ namespace Modules\OffersAndDeals\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Routing\Controller;
+use App\Offer;
 
 class OffersAndDealsController extends Controller
 {
